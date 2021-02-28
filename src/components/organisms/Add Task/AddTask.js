83d7fe3703980a738adfce067a5224f52ace4f17
xfreshu <lprzybyszcontact@gmail.com>
2021-02-28 20:26:04 +0100

    finished task from

--- src/components/organisms/Add Task/AddTask.js
+++ src/components/organisms/Add Task/AddTask.js
@@ -1,13 +1,53 @@
 import React from 'react'
+import styled from 'styled-components'
+import {theme} from "../../Root/Theme";
+
+const Wrapper = styled.form`
+display: flex;
+flex-direction: column;
+width: 100%;
+justify-content: center;
+align-items: center;
+font-size: 20px;
+height: 70%;
+position: absolute;
+top: 50%;
+transform: translateY(-50%);
+`
+
+const Input = styled.input`
+background-color: transparent;
+outline: none;
+border: none;
+color: ${theme.colors.specialText};
+font-weight: 700;
+border-bottom: solid 2px ${theme.colors.text};
+margin-bottom: 70px;
+width: 80%;
+font-size: 20px;
+`
+
+const Button = styled.button`
+padding: 10px 20px;
+background-color: transparent;
+border: solid 2px ${theme.colors.text};
+color: ${theme.colors.text};
+border-radius: 10px;
+transition: .2s;
+:hover{
+background-color: ${theme.colors.text};
+color: white;
+}
+`
 
 const AddTask = () => {
 
     return(
-        <form>
-            <label>123
-                <input type="text"/>
-            </label>
-        </form>
+        <Wrapper>
+                <Input type="text" placeholder='Your task'/>
+                <Input type="text" placeholder='Date'/>
+                <Button type='submit'>Add Task</Button>
+        </Wrapper>
     )
 }
 
